1dc51b5c6a8dc428c27c84991c8f7791a371d16a
Jozef <jozefw@sbcglobal.net>
2014-05-02 17:15:06 -0700

    first commit on Swear app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swear::Application.config.secret_key_base = 'f191c2a1dac1d9a0a56e4a1b938dae1d8bdedaa98d8e3706b188c5e7974260a413d5ef19100d5eaa3a12d66d639530c6d71c12bb056aea06626ee16dcf1d2139'
